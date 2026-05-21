72753917d8e639fc9362f9e72b065f9463a776dc
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2026-05-21 12:34:10 +0200

    Added checks if .env variables provided

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const connectionString = process.env.DATABASE_URL
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is required')
+}
+
+const adapter = new PrismaPg({ connectionString })
 
 export const prisma = new PrismaClient({ adapter })

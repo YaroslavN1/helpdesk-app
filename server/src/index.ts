dd3188a8d9125ef320c1628af4572340aed59c68
Yaroslav Nykyforuk <yaroslav.nykyforuk@gmail.com>
2026-05-21 12:32:45 +0200

    Added Helmet.js

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import { toNodeHandler } from 'better-auth/node'
 import { auth } from './lib/auth'
 import { prisma } from './lib/prisma'
@@ -7,7 +8,8 @@ import { prisma } from './lib/prisma'
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(helmet())
+app.use(cors({ origin: process.env.TRUSTED_ORIGIN ?? 'http://localhost:5173', credentials: true }))
 
 // Auth handler must be registered before express.json()
 app.all('/api/auth/*splat', toNodeHandler(auth))
